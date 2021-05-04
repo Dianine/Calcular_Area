524c074341e496d8482d1405813e4217f03717ef
diane <dianine23@gmail.com>
2021-05-04 20:24:29 -0300

    _4Projeto

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int a = x + y;
+		
+		System.out.printf("Resultado: %d ", a);
+		
+		
+		sc.close();
+		
+		
+
+	}
+
+}
